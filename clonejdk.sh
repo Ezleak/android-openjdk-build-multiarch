4f04950225ab20b312a5db9458d45a2c93f4695a
Ezleak <116900642+Ezleak@users.noreply.github.com>
2023-09-13 21:17:07 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-git clone -b riscv-port --depth 1 https://github.com/openjdk/riscv-port-jdk11u azulu
+git clone -b riscv-port --depth 1 https://github.com/openjdk/riscv-port-jdk11u hg.openjdk
