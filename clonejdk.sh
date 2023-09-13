d1802a95c4a1c6ca9f11bac563d0c6cb353312f9
Ezleak <116900642+Ezleak@users.noreply.github.com>
2023-09-13 20:00:40 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-git clone -b openj9 --depth 1 https://github.com/ibmruntimes/openj9-openjdk-jdk17 openj9
+git clone -b openj9 --depth 1 https://github.com/ibmruntimes/openj9-openjdk-jdk17 openjdk
 bash -b openj9 --depth 1 https://GitHub.com/ibmruntimes/openj9-openjdk-jdk17/get_source.sh
