3c7217c6dd78db192ba84b5f5642e12664fa9274
Ezleak <116900642+Ezleak@users.noreply.github.com>
2023-09-30 13:08:17 +0800

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-git clone -b openj9 --depth 1 https://github.com/ibmruntimes/semeru-openjdk-jdk17 oracle
+git clone -b openj9 --depth 1 https://github.com/ibmruntimes/semeru-openjdk-jdk17 azul
